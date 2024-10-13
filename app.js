1730a33d4918bb134b0acce6ca946cf435dd7e35
Saki Kirishiro <fling.fafnir.firill.0204@gmail.com>
2024-10-13 21:56:30 +0900

    Added button click event in TypeScript

---
+++ app.js
@@ -0,0 +1,4 @@
+var button = document.querySelector('button');
+button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'lightgreen';
+});
